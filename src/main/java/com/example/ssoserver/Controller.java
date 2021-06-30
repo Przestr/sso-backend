d07145a15282d437388d8faf24c17ab3b5bce282
Przestr <struminski.przemyslaw@gmail.com>
2021-06-30 14:25:50 +0200

    Initial commit

---
+++ src/main/java/com/example/ssoserver/Controller.java
@@ -0,0 +1,62 @@
+package com.example.ssoserver;
+
+
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.keycloak.representations.IDToken;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@RestController
+public class Controller {
+
+    @GetMapping(path="/user")
+    public String getResource(){
+        return "protected resource";
+    }
+
+    @GetMapping(path="/test")
+    public String getTest(){
+        return "test";
+    }
+
+    @GetMapping(path="/admin")
+    public String getAdmin(){
+        return "admin";
+    }
+
+
+    @GetMapping(path = "/user/info", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> getUserInfo(Model model, HttpServletRequest request) {
+        Principal user = request.getUserPrincipal();
+        KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) request.getUserPrincipal();
+        AccessToken accessToken = token.getAccount().getKeycloakSecurityContext().getToken();
+
+        HashMap<String, String> accessTokenData = new HashMap<String, String>();
+        accessTokenData.put("Preferred Username: ", accessToken.getPreferredUsername());
+        accessTokenData.put("Email verified:", accessToken.getEmailVerified().toString());
+        accessTokenData.put("Scope:", accessToken.getScope());
+        accessTokenData.put("Roles:", accessToken.getRealmAccess().getRoles().toString());
+        accessTokenData.put("Acr:", accessToken.getAcr());
+        accessTokenData.put("SessionState:", accessToken.getSessionState());
+        accessTokenData.put("User ID:", user.getName());
+        accessTokenData.put("Type:", accessToken.getType());
+        accessTokenData.put("Preferred Username: ", accessToken.getPreferredUsername());
+        accessTokenData.put("Email:", accessToken.getEmail());
+        accessTokenData.put("Family Name:", accessToken.getFamilyName());
+        accessTokenData.put("Given Name:", accessToken.getGivenName());
+        accessTokenData.put("Issuer:", accessToken.getIssuer());
+
+        return accessTokenData;
+    }
+
+}
